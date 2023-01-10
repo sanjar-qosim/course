aaa02d926b95c76023121914cf9777d74dd76bdb
sanjar-qosim <sanjar171766@gmail.com>
2023-01-10 12:26:26 +0500

    conf

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -2,5 +2,9 @@ package common;
 
 public class Config {
     public static final String BROWSER_AND_PLATFORM = "CHROME";
+//    Clean browser cookies after each iteration
+    public static final Boolean CLEAR_COOKIES = true;
+//    To keep the browser open after all scenartio/tests
+    public static final Boolean HOLD_BROWSER_OPEN = true;
 
 }
---
+++ src/main/java/pages/car_loans/CarLoansPage.java
@@ -0,0 +1,10 @@
+package pages.car_loans;
+
+import org.openqa.selenium.WebDriver;
+
+public class CarLoansPage {
+    WebDriver driver;
+    public CarLoansPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
