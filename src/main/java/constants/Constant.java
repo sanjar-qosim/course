2e08935afe651db4540f49f30cc583226b1eb3f6
sanjar-qosim <sanjar171766@gmail.com>
2023-01-11 18:07:54 +0500

    basepage

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -3,5 +3,6 @@ package constants;
 public class Constant {
     public static class TimoutVariables{
         public static final int IMPLICIT_WAIT = 5;
+        public static final int EXPLICIT_WAIT = 10;
     }
 }
